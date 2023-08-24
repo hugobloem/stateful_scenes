d5144b8543fafcde65c258572f6319d5e2b3e14f
Hugo <42470993+hugobloem@users.noreply.github.com>
2023-08-24 19:46:07 +0100

    update default entity name

--- custom_components/stateful_scenes/switch.py
+++ custom_components/stateful_scenes/switch.py
@@ -64,7 +64,7 @@ class StatefulSceneSwitch(SwitchEntity):
         """Initialize an AwesomeLight."""
         self._scene = scene
         self._is_on = None
-        self._name = scene.name
+        self._name = scene.name + " Stateful Scene"
         self._attr_unique_id = "stateful_" + scene.id
 
     @property
