e9b182a5c273a17cdb6135dd56e640cca8df7d0e
Hugo Bloem <hugobloem@gmail.com>
2024-02-21 17:59:05 +0000

    Default to None if no icon in scene.yaml

--- custom_components/stateful_scenes/StatefulScenes.py
+++ custom_components/stateful_scenes/StatefulScenes.py
@@ -95,7 +95,7 @@ class Hub:
         return {
             "name": scene_conf["name"],
             "id": scene_conf["id"],
-            "icon": scene_conf["icon"],
+            "icon": scene_conf["icon"] if "icon" in scene_conf else None,
             "entity_id": entity_id,
             "entities": entities,
         }
